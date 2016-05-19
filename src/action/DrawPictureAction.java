63c17535e58d324ab7bb9bbd504ed6e65e671789
Sen Hou <sen.hou1987@gmail.com>
2016-05-19 17:15:39 +1000

    add more function

---
+++ src/action/DrawPictureAction.java
@@ -0,0 +1,17 @@
+package action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class DrawPictureAction extends ActionSupport{
+	
+	
+	public void drawPicture(){
+		drawWeekSalesPicture();
+	}
+	
+	
+	public void drawWeekSalesPicture(){
+		
+	}
+	
+}
